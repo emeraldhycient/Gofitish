6c81e4edc7be27c8ece854b9fa6dd1da201bebfc
emerald <igwezehycient86@gmail.com>
2022-01-28 00:17:24 +0100

    first getting started screen done

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+
+import Gettingstarted from './screens/auths/gettingstarted';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Gettingstarted/>
+    </PaperProvider>
   );
 }
 
---
+++ screens/auths/gettingstarted.js
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  Dimensions,
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Button } from "react-native-paper";
+
+const Gettingstarted = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("../../assets/images/bentover.jpg")}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <View style={styles.btn_container}>
+          <Button
+            mode="contained"
+            style={[styles.btn, styles.btn1]}
+            labelStyle={{ fontWeight: "700" }}
+            onPress={() => console.log("Pressed")}
+          >
+            SignIn
+          </Button>
+          <Button
+            mode="contained"
+            style={[styles.btn, styles.btn2]}
+            color="#000"
+            labelStyle={{ color: "green", fontWeight: "700" }}
+            onPress={() => console.log("Pressed")}
+          >
+            SignUp
+          </Button>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default Gettingstarted;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+    height: Dimensions.get("screen").height,
+    width: "100%",
+  },
+  btn_container: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 100,
+  },
+  btn: {
+    width: 200,
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 20,
+  },
+  btn1: {
+    backgroundColor: "green",
+  },
+});
