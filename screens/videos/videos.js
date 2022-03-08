5d7e68e350b1a5e16fb902f117e36824c958af76
emeraldhycient <igwezehycient86@gmail.com>
2022-03-08 16:09:26 +0100

    added scrollview to video banner container

--- screens/videos/videos.js
+++ screens/videos/videos.js
@@ -21,29 +21,37 @@ const Videos = () => {
           <Text style={styles.litteheader}>Push up</Text>
           <Text style={styles.litteheader}>Workout</Text>
         </View>
-        <ScrollView>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-around",
-              flexWrap: "wrap",
-              alignItems: "center",
-              marginTop: 10,
-              marginBottom: 30,
-              paddingBottom: 100,
-            }}
-          >
-            <Videobanner />
-            <Videobanner />
-            <Videobanner />
-            <Videobanner />
-            <Videobanner />
-            <Videobanner />
-            <Videobanner />
-            <Videobanner />
-            <Videobanner />
-          </View>
-        </ScrollView>
+        <View style={{ height: 1000 }}>
+          <ScrollView>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-around",
+                flexWrap: "wrap",
+                alignItems: "center",
+                marginTop: 10,
+                marginBottom: 100,
+                paddingBottom: 400,
+              }}
+            >
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+              <Videobanner />
+            </View>
+          </ScrollView>
+        </View>
       </View>
     </SafeAreaView>
   );
